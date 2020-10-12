8f523516cc1885a8cf7a016e838a485cc2757101
dkawasth <dkawasth@durgesh-pc.ro.oracle.com>
2020-10-12 20:30:06 +0530

    performance improve

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { FilterComponent } from './filter/filter.component';
 import { HomeComponent } from './home/home.component';
-import { LaunchListComponent } from './launch-list/launch-list.component';
 import { DashboardService } from './service/dashboard.service';
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-  declarations: [FilterComponent, LaunchListComponent, HomeComponent],
+  declarations: [FilterComponent, HomeComponent],
   imports: [CommonModule, DashboardRoutingModule],
   providers: [DashboardService,HttpClientModule],
 })
